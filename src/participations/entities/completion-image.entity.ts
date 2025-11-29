fcba08ed5e0a4a5b0b4692f21bfcc9e3ed8a3a67
Gabo1234567890 <gabonenov@gmail.com>
2025-11-29 15:10:57 +0200

    Completion image entity for when a challenge is finished added

---
+++ src/participations/entities/completion-image.entity.ts
@@ -0,0 +1,23 @@
+import type { UUID } from 'crypto';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('completion-image')
+export class CompletionImage {
+  @PrimaryGeneratedColumn('uuid')
+  id: UUID;
+
+  @Column()
+  participationId: UUID;
+
+  @Column()
+  userId: UUID;
+
+  @Column()
+  challengeId: UUID;
+
+  @Column()
+  url: string;
+
+  @Column({ type: 'timestamptz' })
+  uploadedAt: Date;
+}
